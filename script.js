b524d4fda68f258f76dec99f2465b65b0f65a261
Micolu101 <104153560+Micolu101@users.noreply.github.com>
2022-06-24 11:56:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let firstName = "Micheal";
+let lastName = "Sabitu";
+let country = "Nigeria";
+let height = "172cm";
+
+document.write("FirstName: "+ firstName, "<br/>");
+document.write("LastName: "+ lastName, "<br/>");
+document.write("Height: "+ height, "<br/>");
+document.write("Country: "+ country, "<br/>");
+
+
+console.log( "FirstName: "+ firstName + "\n" + "LastName: "+ lastName + "\n" 
++ "Height: "+ height + "\n" + "Country: "+ country);
+
+alert( "FirstName: "+ firstName + "\n" + "LastName: "+ lastName + "\n" 
++ "Height: "+ height + "\n" + "Country: "+ country);
